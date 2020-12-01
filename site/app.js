ec19d67f0271fdec44fe9dab11cbeb3889598751
unknown <abhishekkumar9304843750@gmail.com>
2020-12-01 10:24:21 +0530

    some changes

--- site/app.js
+++ site/app.js
@@ -7,5 +7,5 @@ price = 5;
 quantity = 14;
 total = price * quantity;
 
-el = document.getElementById("cost");
+el = document.getElementById("cost").innerHTML = "total= " + total;
 el.textcontext = '$' + total;
